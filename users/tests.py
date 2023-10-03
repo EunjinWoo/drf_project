eb7444f51017a016f0a7355c7df74a87be7dcc2c
EunjinWoo <esther0904@naver.com>
2023-10-03 10:02:47 +0900

    registration test code

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+# users/test.py
+
+from django.urls import reverse # name 값을 이용해서 해당 path를 가져오는 코드. 그래서 url이 바뀌더라도 name만 같으면 그냥 사용할 수 있게.
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 # Create your tests here.
+class UserRegistrationTest(APITestCase):
+    def test_registration(self):
+        url = reverse("user_view")
+        user_data = {
+            "username": "testuser",
+            "fullname": "테스터",
+            "email": "test1@testuser.com",
+            "password": "password",
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.status_code, 201)
